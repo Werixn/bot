571ce8d9ff4b0630a716b124e32496277294a341
Wise Colibri <n.buyuklinskiy@gmail.com>
2023-08-09 21:39:27 +0200

    new fix

--- internal/app/commands/commander.go
+++ internal/app/commands/commander.go
@@ -3,11 +3,27 @@ package commands
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/werixn/bot/internal/service/exercise"
+	"github.com/werixn/bot/internal/service/set"
 )
 
 type Commander struct {
 	bot             *tgbotapi.BotAPI
 	exerciseService *exercise.Service
+}
+
+type Commander2 struct {
+	bot        *tgbotapi.BotAPI
+	setService *set.Service
+}
+
+func NewSecondCommander(
+	bot *tgbotapi.BotAPI,
+	setService *set.Service,
+) *Commander2 {
+	return &Commander2{
+		bot:        bot,
+		setService: setService,
+	}
 }
 
 func NewCommander(
